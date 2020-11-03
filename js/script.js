3f5c558d760e63aede3b3fbcec535d71cd14b93a
Olia <good-friday@mail.ru>
2020-11-04 01:13:12 +0300

    Оживила форму обратной связи и карту

---
+++ js/script.js
@@ -0,0 +1,81 @@
+const contactsButton = document.querySelector('.contacts-button');
+const feedbackPopup = document.querySelector('.modal-feedback');
+const feedbackClose = feedbackPopup.querySelector('.modal-close');
+const feedbackForm = feedbackPopup.querySelector('.modal-feedback-form');
+const feedbackName = feedbackPopup.querySelector('.modal-feedback-name');
+const feedbackEmail = feedbackPopup.querySelector('.modal-feedback-email');
+const feedbackText = feedbackPopup.querySelector('.modal-feedback-textarea');
+
+const mapLink = document.querySelector('.contacts-map-link');
+const mapPopup = document.querySelector('.modal-map');
+const mapClose = mapPopup.querySelector('.modal-close');
+
+let isStorageSupport = true;
+let storageName = '';
+let storageEmail = '';
+
+try {
+  storageName = localStorage.getItem("name");
+  storageEmail = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+contactsButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.add('modal-show');
+  if (storageName && storageEmail) {
+    feedbackName.value = storageName;
+    feedbackEmail.value = storageEmail;
+    feedbackText.focus();
+  } else if (storageName) {
+    feedbackName.value = storageName;
+    feedbackEmail.focus();
+  } else {
+    feedbackName.focus();
+  }
+});
+
+feedbackClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove('modal-show');
+  feedbackPopup.classList.remove('modal-error');
+});
+
+feedbackForm.addEventListener('submit', function (evt) {
+  if (!feedbackName.value || !feedbackEmail.value || !feedbackText.value) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove('modal-error');
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+    feedbackPopup.classList.add('modal-error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('name', feedbackName.value);
+      localStorage.setItem('email', feedbackEmail.value);
+    }
+  }
+});
+
+mapLink.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add('modal-show');
+});
+
+mapClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove('modal-show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (feedbackPopup.classList.contains('modal-show')) {
+      evt.preventDefault();
+      feedbackPopup.classList.remove('modal-show');
+      feedbackPopup.classList.remove('modal-error');
+    }
+    if (mapPopup.classList.contains('modal-show')) {
+      evt.preventDefault();
+      mapPopup.classList.remove('modal-show');
+    }
+  }
+});
